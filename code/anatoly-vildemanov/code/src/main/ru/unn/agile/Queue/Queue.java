f8325af3f6298b1dedb6cac6ab96a2e703feb8bc
Anatoly Vildemanov <vildemanov.a@gmail.com>
2013-10-13 20:57:37 +0400

    Lab1 tdd - Queue

--- code/anatoly-vildemanov/code/src/main/ru/unn/agile/Queue/Queue.java
+++ code/anatoly-vildemanov/code/src/main/ru/unn/agile/Queue/Queue.java
@@ -1,81 +1,50 @@
 package ru.unn.agile.Queue;
 
-/**
- * Created with IntelliJ IDEA.
- * User: user
- * Date: 12.10.13
- * Time: 21:33
- * To change this template use File | Settings | File Templates.
- */
 public class Queue {
 
     private int[] queue;
-    private int head;
     private int tail;
+    private int head;
     private int count;
-    private final int SIZE_OF_QUEUE = 10;
+    private final int MAX_LEN = 10;
 
-    public Queue(){
-        queue = new int[SIZE_OF_QUEUE];
-        tail  = -1;
-        head  =  0;
-        count =  0;
-    };
+    public Queue()
+    {
+        queue = new int[MAX_LEN];
+        count = head = 0;
+        tail = -1;
+    }
 
     public boolean isEmpty()
     {
         return (count == 0);
     }
-    public void push(String elements)
+
+    public void push(int element)
     {
-        if (elements == "")
-            return;
-
-        String[] tokens = parseStringToElements(elements);
-
-        for (String token: tokens)
-            pushOneElement(token);
-    }
-
-    public String top()
-    {
-        if (isEmpty())
-            return "queue is empty";
-        return Integer.toString(queue[head]);
-    }
-
-    public String pop()
-    {
-        if (isEmpty())
-            return "queue is empty";
-        count--;
-        int res = queue[head];
-        head = (head + 1) % getMaxSize();
-        return Integer.toString(res);
-    }
-
-    private void pushOneElement(String element)
-    {
-        if (getSize()!= getMaxSize())
+        if (!isFull())
         {
+            tail = (tail + 1) % MAX_LEN;
+            queue[tail] = element;
             count++;
-            tail = (tail + 1) % getMaxSize();
-            queue[tail] = Integer.parseInt(element);
         }
     }
 
-    private String[] parseStringToElements(String string)
+    public int top()
     {
-        return string.split("[,]")  ;
+        return queue[head];
     }
 
-    public int getMaxSize()
+    public int pop()
     {
-        return SIZE_OF_QUEUE;
+       count--;
+       int res = queue[head];
+       head = (head + 1) % MAX_LEN;
+       return res;
     }
 
-    public int getSize()
+    public boolean isFull()
     {
-        return count;
+        return count >= MAX_LEN;
     }
 }
--- code/anatoly-vildemanov/code/src/test/ru/unn/agile/Queue/core/QueueTest.java
+++ code/anatoly-vildemanov/code/src/test/ru/unn/agile/Queue/core/QueueTest.java
@@ -16,112 +16,89 @@ public class QueueTest {
     }
 
     @Test
-    public void createQueueIsEmpty()
+    public void createEmptyQueue()
     {
-        assertEmpty(true);
+        assertEquals(true, queue.isEmpty());
     }
 
     @Test
-    public void pushNullStringQueueIsEmpty()
+    public void pushElement()
     {
-        queue.push("");
-        assertEmpty(true);
+        queue.push(2);
+        assertEquals(false, queue.isEmpty());
     }
 
     @Test
-    public void pushOneElementQueueIsNotEmpty()
+    public void pushManyElements()
     {
-        queue.push("6");
-        assertEmpty(false);
+        fillUpQueue(12);
+        assertEquals(false, queue.isEmpty());
     }
 
     @Test
-    public void pushOneElementItOnTop()
+    public void fullTank()
     {
-        String element = "9";
-        queue.push(element);
-        assertEquals(element, queue.top());
+        fillUpQueue(7);
+        assertEquals(false, queue.isFull());
     }
 
     @Test
-    public void topOneElement()
+    public void lookElement()
     {
-        String elementList="7,4,9";
-        queue.push(elementList);
-        assertEquals("7",queue.top());
+        fillUpQueue(7);
+        assertEquals(0, queue.top());
     }
 
     @Test
-    public void popOneElement()
+    public void takeHeadElement()
     {
-        String elementList="7,4,9";
-        queue.push(elementList);
-        assertEquals("7",queue.pop());
+        fillUpQueue(7);
+        assertEquals(0, queue.pop());
     }
 
     @Test
-    public void pushMoreElementsThanQueue()
+    public void takeHeadElementInOnesElementQueue()
     {
-        String elementList="7,4,9,4,3,2,5,7,5,3,3,4,2,5";
-        queue.push(elementList);
-        assertEquals("7", queue.pop());
+        queue.push(3);
+        queue.pop();
+        assertEquals(true, queue.isEmpty());
+    }
+
+    @Test
+    public void popPushPopHeadElement()
+    {
+        queue.push(3);
+        queue.pop();
+        queue.push(2);
+        assertEquals(2, queue.pop());
     }
 
     @Test
     public void pushListPopPartElements()
     {
-        String elementList="7,4,9";
-        queue.push(elementList);
+        fillUpQueue(3);
         for (int i = 0; i < 2; i++)
             queue.pop();
-        assertEquals("9", queue.pop());
-    }
-
-    @Test
-    public void pushListPopAllElements()
-    {
-        String elementList="7,4,9";
-        queue.push(elementList);
-        for (int i = 0; i < 3; i++)
-            queue.pop();
-
-        assertEmpty(true);
-    }
-
-    @Test
-    public void popEmptyQueue()
-    {
-        assertEmptyError();
-    }
-
-    @Test
-    public void topEmptyQueue()
-    {
-        assertEmptyError();
+        assertEquals(2, queue.pop());
     }
 
     @Test
     public void whenWorkLongTime()
     {
-        String elementList="3,5,2,5,7,4";
         for (int j = 0; j < 2; j++)
         {
-            queue.push(elementList);
-            for (int i = 0; i < 6; i++)
+            fillUpQueue(7);
+            for (int i = 0; i < 7; i++)
                 queue.pop();
         }
-        queue.push(elementList);
-        assertEquals("3", queue.pop());
+        fillUpQueue(7);
+        assertEquals(0, queue.pop());
     }
 
-    private void assertEmpty(boolean expected)
+    private void fillUpQueue(int countOfElement)
     {
-        assertEquals(expected,queue.isEmpty());
-    }
-
-    private void assertEmptyError()
-    {
-        assertEquals("queue is empty", queue.pop());
+        for (int i = 0; i < countOfElement; i++)
+            queue.push(i);
     }
 
 }
