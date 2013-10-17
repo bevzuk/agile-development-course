4f9936278e830acddfc55827d547ee19c1fab3b6
Egor92 <novikov.ea@mail.ru>
2013-10-18 00:25:30 +0400

    Fixed notes

--- code/egor-novikov/core/src/main/ru/unn/agile/creditcalculator/CreditCalculator.java
+++ code/egor-novikov/core/src/main/ru/unn/agile/creditcalculator/CreditCalculator.java
@@ -2,54 +2,122 @@ package ru.unn.agile.creditcalculator;
 
 public class CreditCalculator
 {
-    private final double amount;
+	private final int MONTH_PER_YEAR = 12;
 
-    private final int monthsCount;
+	private final int HUNDRED = 100;
 
-    private final double percent;
+    private double amount;
 
-    public double getAmount()
+    private int monthsCount;
+
+    private double percent;
+
+	public double getAmount()
+	{
+		return amount;
+	}
+
+	public void setAmount(double value)
+	{
+		amount = value;
+	}
+
+	public int getMonthsCount()
+	{
+		return monthsCount;
+	}
+
+	public void setMonthsCount(int value)
+	{
+		monthsCount = value;
+	}
+
+	public double getPercent()
+	{
+		return percent;
+	}
+
+	public void setPercent(double value)
+	{
+		percent = value;
+	}
+
+	public static class Builder
+	{
+		private double amount;
+
+		private int monthsCount;
+
+		private double percent;
+
+		private boolean isAmountSetted;
+
+		private boolean isMonthsCountSetted;
+
+		private boolean isPercentSetted;
+
+		public Builder() {}
+
+		public Builder amount(double value)
+		{
+			checkAmount(value);
+			amount = value;
+			isAmountSetted = true;
+			return this;
+		}
+
+		public Builder monthsCount(int value)
+		{
+			checkMonthsCount(value);
+			monthsCount = value;
+			isMonthsCountSetted = true;
+			return this;
+		}
+
+		public Builder percent(double value)
+		{
+			checkPercent(value);
+			percent = value;
+			isPercentSetted = true;
+			return this;
+		}
+
+		private void checkAmount(double amount)
+		{
+			if (amount <= 0.0)
+				throw new IllegalArgumentException("amount argument value exception");
+		}
+
+		private void checkMonthsCount(double monthsCount)
+		{
+			if (monthsCount <= 0)
+				throw new IllegalArgumentException("monthsCount argument value exception");
+		}
+
+		private void checkPercent(double percent)
+		{
+			if (percent < 0.0)
+				throw new IllegalArgumentException("percent argument value exception");
+		}
+
+		private boolean isAllDataSetted()
+		{
+			return isAmountSetted && isMonthsCountSetted && isPercentSetted;
+		}
+
+		public CreditCalculator build()
+		{
+			if (!isAllDataSetted())
+				throw new IllegalStateException("Not all required fields are filled");
+			return new CreditCalculator(this);
+		}
+	}
+
+    public CreditCalculator(Builder builder)
     {
-        return amount;
-    }
-
-    public int getMonthsCount()
-    {
-        return monthsCount;
-    }
-
-    public double getPercent()
-    {
-        return percent;
-    }
-
-    public CreditCalculator(double amount, int monthsCount, double percent)
-    {
-        checkAmount(amount);
-        checkMonthsCount(monthsCount);
-        checkPercent(percent);
-
-        this.amount = amount;
-        this.monthsCount = monthsCount;
-        this.percent = percent;
-    }
-
-    private void checkAmount(double amount)
-    {
-        if (amount <= 0.0)
-            throw new IllegalArgumentException("amount argument value exception");
-    }
-
-    private void checkMonthsCount(double monthsCount)
-    {
-        if (monthsCount <= 0)
-            throw new IllegalArgumentException("monthsCount argument value exception");
-    }
-
-    private void checkPercent(double percent)
-    {
-        if (percent < 0.0)
-            throw new IllegalArgumentException("percent argument value exception");
+        this.amount = builder.amount;
+        this.monthsCount = builder.monthsCount;
+        this.percent = builder.percent;
     }
 
     private void checkMonthNumber(int monthNumber)
@@ -60,7 +128,7 @@ public class CreditCalculator
 
     public double getAnnuityMonthPayment()
     {
-        double interestRateCoefficient = getPercent()/1200.0;
+        double interestRateCoefficient = getPercent() / (MONTH_PER_YEAR * HUNDRED);
         double annuityCoefficient = interestRateCoefficient*Math.pow(1 + interestRateCoefficient, getMonthsCount())
                                         /(Math.pow(1 + interestRateCoefficient, getMonthsCount()) - 1);
         return annuityCoefficient * getAmount();
@@ -70,7 +138,8 @@ public class CreditCalculator
     {
         checkMonthNumber(monthNumber);
         double monthlyPrincipalPayment = getAmount() / getMonthsCount();
-        return monthlyPrincipalPayment + (getAmount() - monthlyPrincipalPayment*(monthNumber-1)) * getPercent() / 1200.0;
+        return monthlyPrincipalPayment + (getAmount() - monthlyPrincipalPayment*(monthNumber-1)) * getPercent()
+																						/ (MONTH_PER_YEAR * HUNDRED);
     }
 
     public double getAnnuityTotalPayment()
--- code/egor-novikov/core/src/test/ru/unn/agile/creditcalculator/WhenCalculateCredit.java
+++ code/egor-novikov/core/src/test/ru/unn/agile/creditcalculator/WhenCalculateCredit.java
@@ -7,87 +7,199 @@ import static org.junit.Assert.fail;
 
 public class WhenCalculateCredit
 {
-    @Test
-    public void incorrectAmountArgumentThrowExceptionInConstructor()
-    {
-        try
-        {
-            new CreditCalculator(0, 24, 10);
-            fail();
-        }
-        catch (IllegalArgumentException ex)
-        {
-            assertEquals("amount argument value exception", ex.getMessage());
-        }
-    }
+	@Test
+	public void zeroAmountArgumentThrowExceptionInBuilder()
+	{
+		try
+		{
+			new CreditCalculator.Builder().amount(0).monthsCount(15).percent(5).build();
+			fail();
+		}
+		catch (IllegalArgumentException ex)
+		{
+			assertEquals("amount argument value exception", ex.getMessage());
+		}
+	}
 
-    @Test
-    public void incorrectMonthsCountArgumentThrowExceptionInConstructor()
-    {
-        try
-        {
-            new CreditCalculator(180000, 0, 10);
-            fail();
-        }
-        catch (IllegalArgumentException ex)
-        {
-            assertEquals("monthsCount argument value exception", ex.getMessage());
-        }
-    }
+	@Test
+	public void negativeAmountArgumentThrowExceptionInBuilder()
+	{
+		try
+		{
+			new CreditCalculator.Builder().amount(-10000).monthsCount(15).percent(5).build();
+			fail();
+		}
+		catch (IllegalArgumentException ex)
+		{
+			assertEquals("amount argument value exception", ex.getMessage());
+		}
+	}
 
-    @Test
-    public void incorrectPercentArgumentThrowExceptionInConstructor()
-    {
-        try
-        {
-            new CreditCalculator(180000, 24, -10.0);
-            fail();
-        }
-        catch (IllegalArgumentException ex)
-        {
-            assertEquals("percent argument value exception", ex.getMessage());
-        }
-    }
+	@Test
+	public void zeroMonthsCountArgumentThrowExceptionInBuilder()
+	{
+		try
+		{
+			new CreditCalculator.Builder().amount(145000).monthsCount(0).percent(14).build();
+			fail();
+		}
+		catch (IllegalArgumentException ex)
+		{
+			assertEquals("monthsCount argument value exception", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void negativeMonthsCountArgumentThrowExceptionInConstructor()
+	{
+		try
+		{
+			new CreditCalculator.Builder().amount(125000).monthsCount(-12).percent(19).build();
+			fail();
+		}
+		catch (IllegalArgumentException ex)
+		{
+			assertEquals("monthsCount argument value exception", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void negativePercentArgumentThrowExceptionInConstructor()
+	{
+		try
+		{
+			new CreditCalculator.Builder().amount(75000).monthsCount(36).percent(-10).build();
+			fail();
+		}
+		catch (IllegalArgumentException ex)
+		{
+			assertEquals("percent argument value exception", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void BuilderThrowExceptionIfNotAllRequiredFieldsAreFilled1()
+	{
+		try
+		{
+			new CreditCalculator.Builder().amount(20000).build();
+			fail();
+		}
+		catch (IllegalStateException ex)
+		{
+			assertEquals("Not all required fields are filled", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void BuilderThrowExceptionIfNotAllRequiredFieldsAreFilled2()
+	{
+		try
+		{
+			new CreditCalculator.Builder().monthsCount(10).build();
+			fail();
+		}
+		catch (IllegalStateException ex)
+		{
+			assertEquals("Not all required fields are filled", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void BuilderThrowExceptionIfNotAllRequiredFieldsAreFilled3()
+	{
+		try
+		{
+			new CreditCalculator.Builder().percent(11).build();
+			fail();
+		}
+		catch (IllegalStateException ex)
+		{
+			assertEquals("Not all required fields are filled", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void BuilderThrowExceptionIfNotAllRequiredFieldsAreFilled4()
+	{
+		try
+		{
+			new CreditCalculator.Builder().amount(20000).monthsCount(12).build();
+			fail();
+		}
+		catch (IllegalStateException ex)
+		{
+			assertEquals("Not all required fields are filled", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void BuilderThrowExceptionIfNotAllRequiredFieldsAreFilled5()
+	{
+		try
+		{
+			new CreditCalculator.Builder().amount(20000).percent(6).build();
+			fail();
+		}
+		catch (IllegalStateException ex)
+		{
+			assertEquals("Not all required fields are filled", ex.getMessage());
+		}
+	}
+
+	@Test
+	public void BuilderThrowExceptionIfNotAllRequiredFieldsAreFilled6()
+	{
+		try
+		{
+			new CreditCalculator.Builder().monthsCount(12).percent(7).build();
+			fail();
+		}
+		catch (IllegalStateException ex)
+		{
+			assertEquals("Not all required fields are filled", ex.getMessage());
+		}
+	}
 
     @Test
     public void correctData1WorksProperlyInGetAnnuityMonthPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(120000, 12, 24);
+        CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(120000).monthsCount(12).percent(24).build();
         assertDoubles(11347.15, creditCalculator.getAnnuityMonthPayment());
     }
 
     @Test
     public void correctData2WorksProperlyInGetAnnuityMonthPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(100000, 36, 10);
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(100000).monthsCount(36).percent(10).build();
         assertDoubles(3226.72, creditCalculator.getAnnuityMonthPayment());
     }
 
     @Test
     public void correctData3WorksProperlyInGetAnnuityMonthPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(400000, 11, 7);
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(400000).monthsCount(11).percent(7).build();
         assertDoubles(37648.70, creditCalculator.getAnnuityMonthPayment());
     }
 
     @Test
     public void correctData1WorksProperlyInGetDifferentiatedMonthPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(120000, 12, 24);
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(120000).monthsCount(12).percent(24).build();
         assertDoubles(11600.00, creditCalculator.getDifferentiatedMonthPayment(5));
     }
 
     @Test
     public void correctData2WorksProperlyInGetDifferentiatedMonthPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(120000, 12, 24);
-        assertDoubles(11000.00, creditCalculator.getDifferentiatedMonthPayment(8));
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(140000).monthsCount(17).percent(18.5).build();
+        assertDoubles(8616.18 , creditCalculator.getDifferentiatedMonthPayment(15));
     }
 
     @Test
     public void correctData3WorksProperlyInGetDifferentiatedMonthPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(170000, 6, 5);
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(170000).monthsCount(6).percent(5).build();
         assertDoubles(28805.56, creditCalculator.getDifferentiatedMonthPayment(3));
     }
 
@@ -96,7 +208,7 @@ public class WhenCalculateCredit
     {
         try
         {
-            CreditCalculator creditCalculator = new CreditCalculator(180000, 24, 16);
+			CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(180000).monthsCount(24).percent(16).build();
             creditCalculator.getDifferentiatedMonthPayment(0);
             fail();
         }
@@ -111,7 +223,7 @@ public class WhenCalculateCredit
     {
         try
         {
-            CreditCalculator creditCalculator = new CreditCalculator(180000, 24, 16);
+			CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(160000).monthsCount(24).percent(11).build();
             creditCalculator.getDifferentiatedMonthPayment(24+1);
             fail();
         }
@@ -124,42 +236,42 @@ public class WhenCalculateCredit
     @Test
     public void correctData1WorksProperlyInGetAnnuityTotalPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(120000, 12, 24);
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(120000).monthsCount(12).percent(24).build();
         assertDoubles(136165.82, creditCalculator.getAnnuityTotalPayment());
     }
 
     @Test
     public void correctData2WorksProperlyInGetAnnuityTotalPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(180000, 18, 10);
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(180000).monthsCount(18).percent(10).build();
         assertDoubles(194584.94, creditCalculator.getAnnuityTotalPayment());
     }
 
     @Test
     public void correctData3WorksProperlyInGetAnnuityTotalPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(35000, 10, 15);
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(35000).monthsCount(10).percent(15).build();
         assertDoubles(37451.08, creditCalculator.getAnnuityTotalPayment());
     }
 
     @Test
     public void correctData1WorksProperlyInGetDifferentiatedTotalPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(180000, 18, 10);
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(180000).monthsCount(18).percent(10).build();
         assertDoubles(194250.00, creditCalculator.getDifferentiatedTotalPayment());
     }
 
     @Test
     public void correctData2WorksProperlyInGetDifferentiatedTotalPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(200000, 6, 5);
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(200000).monthsCount(6).percent(5).build();
         assertDoubles(202916.67, creditCalculator.getDifferentiatedTotalPayment());
     }
 
     @Test
     public void correctData3WorksProperlyInGetDifferentiatedTotalPaymentMethod()
     {
-        CreditCalculator creditCalculator = new CreditCalculator(150000, 9, 15);
+		CreditCalculator creditCalculator = new CreditCalculator.Builder().amount(150000).monthsCount(9).percent(15).build();
         assertDoubles(159375.00, creditCalculator.getDifferentiatedTotalPayment());
     }
 
