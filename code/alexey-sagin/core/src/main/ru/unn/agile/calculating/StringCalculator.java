bea3caefa954824d33fd7392b823ff1184fd7482
Alexey Sagin <neutralnn@gmail.com>
2013-10-18 12:56:56 +0400

    added tests, refactoring tests, automatically refromat with IntelliJ

--- code/alexey-sagin/core/src/main/ru/unn/agile/calculating/StringCalculator.java
+++ code/alexey-sagin/core/src/main/ru/unn/agile/calculating/StringCalculator.java
@@ -5,36 +5,47 @@ import java.util.Stack;
 
 public class StringCalculator {
 
+    private static final String numericSymbols = "0123456789.";
+    private static final String opSymbols = "+-*/";
+    private static final String allowedSymbols = numericSymbols + opSymbols + "()\t\r\n ";
+    private static final HashMap<Character, Integer> opPriors = new HashMap<Character, Integer>();
+
+    static {
+        opPriors.put('+', 1);
+        opPriors.put('-', 1);
+        opPriors.put('*', 2);
+        opPriors.put('/', 2);
+        opPriors.put('(', 0);
+        opPriors.put(')', 0);
+    }
+
     public static double calculate(String expr) {
-
         if (expr.isEmpty()) return 0;
-
         checkSymbols(expr);
         checkParens(expr);
         checkOps(expr);
-
         Stack<Double> stackArgs = new Stack<Double>();
         Stack<Character> stackOps = new Stack<Character>();
-
         while (!expr.isEmpty()) {
             char firstSym = expr.charAt(0);
             if (isNumericSymbol(firstSym)) {
                 expr = pushNumAndTruncateString(expr, stackArgs);
-            }else if (isOpSymbol(firstSym) || firstSym == '(' || firstSym == ')'){
+            } else if (isOpSymbol(firstSym) || firstSym == '(' || firstSym == ')') {
                 expr = processOpAndTruncateString(expr, stackArgs, stackOps);
-            }else {
+            } else {
                 expr = expr.substring(1);
             }
         }
         while (!stackOps.isEmpty()) {
-            performOneOp(stackArgs,stackOps);
+            performOneOp(stackArgs, stackOps);
         }
-        if (stackArgs.size() > 1) throw new IllegalArgumentException("Not enough operators in expression");
+        if (stackArgs.size() > 1)
+            throw new IllegalArgumentException("Not enough operators in expression");
         return stackArgs.pop();
     }
 
     private static void checkSymbols(String expr) {
-        for (int i=0; i<expr.length(); i++) {
+        for (int i = 0; i < expr.length(); i++) {
             if (!isAllowedSymbol(expr.charAt(i))) {
                 throw new IllegalArgumentException("Undefined symbol in expression");
             }
@@ -43,7 +54,7 @@ public class StringCalculator {
 
     private static void checkParens(String expr) {
         Stack<Character> stack = new Stack<Character>();
-        for (int i=0; i<expr.length(); i++) {
+        for (int i = 0; i < expr.length(); i++) {
             if ('(' == expr.charAt(i)) {
                 stack.push('(');
             }
@@ -59,17 +70,17 @@ public class StringCalculator {
     }
 
     private static void checkOps(String expr) {
-        for (int i=0; i<expr.length(); i++)
+        for (int i = 0; i < expr.length(); i++)
             if (isOpSymbol(expr.charAt(i))) {
-                if (i==0 || i==expr.length())
+                if (i == 0 || i == expr.length())
                     throw new IllegalArgumentException("Wrong operator position in expression");
-                int index = i-1;
+                int index = i - 1;
                 while (index >= 0 && !isNumericSymbol(expr.charAt(index))
                         && !isOpSymbol(expr.charAt(index)))
                     index--;
                 if (index < 0 || isOpSymbol(expr.charAt(index)))
                     throw new IllegalArgumentException("Wrong operator position in expression");
-                index = i+1;
+                index = i + 1;
                 while (index < expr.length() && !isNumericSymbol(expr.charAt(index))
                         && !isOpSymbol(expr.charAt(index)))
                     index++;
@@ -84,7 +95,7 @@ public class StringCalculator {
                 && isNumericSymbol(expr.charAt(numLength))) {
             numLength++;
         }
-        String strNum = expr.substring(0,numLength);
+        String strNum = expr.substring(0, numLength);
         double num = Double.parseDouble(strNum);
         stackArgs.push(num);
         return expr.substring(numLength);
@@ -134,32 +145,15 @@ public class StringCalculator {
         }
     }
 
-    private static final String numericSymbols = "0123456789.";
-
     private static boolean isNumericSymbol(char ch) {
         return numericSymbols.indexOf(ch) >= 0;
     }
-
-    private static final String opSymbols = "+-*/";
 
     private static boolean isOpSymbol(char ch) {
         return opSymbols.indexOf(ch) >= 0;
     }
 
-    private static final String allowedSymbols = numericSymbols + opSymbols + "()\t\r\n ";
-
     private static boolean isAllowedSymbol(char ch) {
         return allowedSymbols.indexOf(ch) >= 0;
-    }
-
-    private static final HashMap<Character,Integer> opPriors = new HashMap<Character, Integer>();
-
-    static {
-        opPriors.put('+',1);
-        opPriors.put('-',1);
-        opPriors.put('*',2);
-        opPriors.put('/',2);
-        opPriors.put('(',0);
-        opPriors.put(')',0);
     }
 }
--- code/alexey-sagin/core/src/test/ru/unn/agile/calculating/StringCalculatorTest.java
+++ code/alexey-sagin/core/src/test/ru/unn/agile/calculating/StringCalculatorTest.java
@@ -7,136 +7,151 @@ import static org.junit.Assert.fail;
 
 public class StringCalculatorTest {
 
+    private final double calcError = 1e-10;
+    private final String wrongOpsExceptionText = "Wrong operator position in expression";
+    private final String wrongParensExceptionText = "Wrong parens in expression";
+    private final String fewOpsExceptionText = "Not enough operators in expression";
+    private final String notAllowedSymbolsExceptionText = "Undefined symbol in expression";
+
     @Test
     public void emptyStringGivesZero() {
         double res = StringCalculator.calculate("");
-        assertEquals(0.0, res, 1e-10);
+        assertEquals(0.0, res, calcError);
     }
 
     @Test
     public void oneNumberGivesNumber() {
         double res = StringCalculator.calculate("5");
-        assertEquals(5.0, res, 1e-10);
+        assertEquals(5.0, res, calcError);
     }
 
     @Test
     public void oneRealNumberGivesRealNumber() {
         double res = StringCalculator.calculate("5.7");
-        assertEquals(5.7, res, 1e-10);
+        assertEquals(5.7, res, calcError);
     }
 
     @Test
     public void sumGivesSum() {
         double res = StringCalculator.calculate("5+5");
-        assertEquals(5.0+5, res, 1e-10);
+        assertEquals(5.0 + 5, res, calcError);
     }
 
     @Test
     public void wrongSymbolsGivesException() {
-        try {
-            StringCalculator.calculate("5+a");
-            fail("Exception must be thrown");
-        } catch (IllegalArgumentException e) {
-            assertEquals("Undefined symbol in expression", e.getMessage());
-        } catch (Exception e) {
-            fail("Wrong exception type");
-        }
+        checkExprGivesException("5+a", notAllowedSymbolsExceptionText);
     }
 
     @Test
     public void sumWithSpacesGivesSum() {
         double res = StringCalculator.calculate("5  +5  ");
-        assertEquals(5.0+5, res, 1e-10);
+        assertEquals(5.0 + 5, res, calcError);
     }
 
     @Test
     public void sumWithParensGivesSum() {
         double res = StringCalculator.calculate("2+(5+5)");
-        assertEquals(2.0+(5+5), res, 1e-10);
+        assertEquals(2.0 + (5 + 5), res, calcError);
     }
 
     @Test
     public void checkOperationPriorsWithParens() {
         double res = StringCalculator.calculate(" 2 - (5+5)");
-        assertEquals(2.0-(5+5), res, 1e-10);
+        assertEquals(2.0 - (5 + 5), res, calcError);
     }
 
     @Test
     public void checkMultiply() {
         double res = StringCalculator.calculate(" 2 * (5+5)");
-        assertEquals(2.0*(5+5), res, 1e-10);
+        assertEquals(2.0 * (5 + 5), res, calcError);
     }
 
     @Test
     public void checkDivision() {
         double res = StringCalculator.calculate(" 2 / (5+5*7)");
-        assertEquals(2.0/(5+5*7), res, 1e-10);
+        assertEquals(2.0 / (5 + 5 * 7), res, calcError);
     }
 
     @Test
     public void wrongParensGivesException() {
-        try {
-            StringCalculator.calculate("5+5)");
-            fail("Exception must be thrown");
-        } catch (IllegalArgumentException e) {
-            assertEquals("Wrong parens in expression", e.getMessage());
-        } catch (Exception e) {
-            fail("Wrong exception type");
-        }
+        checkExprGivesException("5+5)", wrongParensExceptionText);
     }
 
     @Test
     public void checkComplexExpression() {
         double res = StringCalculator.calculate("(1.1-2*7)/19+1+2*(2+2/2)");
-        assertEquals((1.1-2*7)/19+1+2*(2+2/2), res, 1e-10);
+        assertEquals((1.1 - 2 * 7) / 19 + 1 + 2 * (2 + 2 / 2), res, calcError);
     }
 
     @Test
     public void tooFewOpsGivesException() {
-        try {
-            StringCalculator.calculate("5 5");
-            fail("Exception must be thrown");
-        } catch (IllegalArgumentException e) {
-            assertEquals("Not enough operators in expression", e.getMessage());
-        } catch (Exception e) {
-            fail("Wrong exception type");
-        }
+        checkExprGivesException("5 5", fewOpsExceptionText);
+
     }
 
     @Test
     public void opInBeginGivesException() {
-        try {
-            StringCalculator.calculate("+5 5");
-            fail("Exception must be thrown");
-        } catch (IllegalArgumentException e) {
-            assertEquals("Wrong operator position in expression", e.getMessage());
-        } catch (Exception e) {
-            fail("Wrong exception type");
-        }
+        checkExprGivesException("+5 5", wrongOpsExceptionText);
     }
 
     @Test
     public void opInEndGivesException() {
-        try {
-            StringCalculator.calculate("5 5 + ");
-            fail("Exception must be thrown");
-        } catch (IllegalArgumentException e) {
-            assertEquals("Wrong operator position in expression", e.getMessage());
-        } catch (Exception e) {
-            fail("Wrong exception type");
-        }
+        checkExprGivesException("5 5 + ", wrongOpsExceptionText);
     }
 
     @Test
     public void wrongOpGivesException() {
-        try {
-            StringCalculator.calculate("5 6 + * 5 ");
-            fail("Exception must be thrown");
-        } catch (IllegalArgumentException e) {
-            assertEquals("Wrong operator position in expression", e.getMessage());
-        } catch (Exception e) {
-            fail("Wrong exception type");
-        }
+        checkExprGivesException("5 6 + * 5 ", wrongOpsExceptionText);
     }
 
+    @Test
+    public void checkOpsPriorWithDivisionAndMinus() {
+        double res = StringCalculator.calculate("1/1-1");
+        assertEquals(1.0 / 1 - 1, res, calcError);
+    }
+
+    @Test
+    public void checkOpsPriorWithDivisionAndMinusAndParens() {
+        double res = StringCalculator.calculate("2/(2-2)");
+        assertEquals(2.0 / (2 - 2), res, calcError);
+    }
+
+    @Test
+    public void checkOneOpGiveException() {
+        checkExprGivesException("+", wrongOpsExceptionText);
+    }
+
+    @Test
+    public void checkUnaryMinusIsNotAllowed() {
+        checkExprGivesException("-1", wrongOpsExceptionText);
+    }
+
+    @Test
+    public void checkOpsPriorParseCorrectly() {
+        double res = StringCalculator.calculate("2/1 + 2*8 - (2.7+3)");
+        assertEquals(2.0 / 1 + 2 * 8 - (2.7 + 3), res, calcError);
+    }
+
+    @Test
+    public void checkDivisionIsLeftAssoc() {
+        double res = StringCalculator.calculate("2/2/2/2");
+        assertEquals(((2.0 / 2) / 2) / 2, res, calcError);
+    }
+
+    @Test
+    public void checkMinusIsLeftAssoc() {
+        double res = StringCalculator.calculate("2-2-2-2");
+        assertEquals(((2.0 - 2) - 2) - 2, res, calcError);
+    }
+
+    private void checkExprGivesException(String expr, String exceptionText) {
+        try {
+            StringCalculator.calculate(expr);
+            fail("Exception must be thrown.");
+        } catch (IllegalArgumentException e) {
+            assertEquals(exceptionText, e.getMessage());
+        } catch (Exception e) {
+            fail("Wrong exception type.");
+        }
+    }
 }
